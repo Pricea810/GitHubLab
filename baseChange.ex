c3d7ddae42f4cce6097da4fda92e17700289f080
shivanerathore <77806349+shivanerathore@users.noreply.github.com>
2021-01-26 15:41:27 -0500

    Update baseChange.ex

--- baseChange.ex
+++ baseChange.ex
@@ -1,7 +1,8 @@
 defmodule BaseChange do
   #Convert takes in a string that reporesents a number, an integer that represents the base that said number is in, and another integer that indicates which base you would like to switch to.
+  #adding comment bc i dont wanna actually change your code - interesting program
   @spec convert(string,integer,integer) :: string
-  def convert(num, baseA, baseB) do
+  def convert(num, baseA, baseB) do 
       baseTen = Integer.parse(num, baseA)|>Tuple.to_list()|>List.first()
       Integer.to_string(baseTen, baseB)
   end
